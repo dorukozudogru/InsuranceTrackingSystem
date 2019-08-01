a48d34a0bfc09c6038de8618e416945cb1af0fe2
Doruk Özüdoğru <doruk.ozudogru@oplog.com.tr>
2019-08-01 17:17:17 +0300

    denemeler yapılıyor

---
+++ Controllers/TestController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace SigortaTakipSistemi.Controllers
+{
+    public class TestController : Controller
+    {
+        public IActionResult ServerSidePagination()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult ServerSidePagination(int asd)
+        {
+            return View();
+        }
+    }
+}
