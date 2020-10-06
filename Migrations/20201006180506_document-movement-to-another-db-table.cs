e2eb6196a1964d6c9c0d1895179fd3aaf436ab92
Doruk Özüdoğru <dorukozudogru@hotmail.com>
2020-10-06 21:06:24 +0300

    Migration Added

---
+++ Migrations/20201006180506_document-movement-to-another-db-table.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace SigortaTakipSistemi.Migrations
+{
+    public partial class documentmovementtoanotherdbtable : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "InsuranceDocuments",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    InsuranceId = table.Column<int>(nullable: false),
+                    Document = table.Column<byte[]>(nullable: true),
+                    DocumentName = table.Column<string>(nullable: true),
+                    DocumentFormat = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_InsuranceDocuments", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "InsuranceDocuments");
+        }
+    }
+}
